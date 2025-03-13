c0e3bbb8fc2707997f0817046c7da2677870d4a4
matsuo1234567 <122419523+matsuo1234567@users.noreply.github.com>
2025-03-14 03:07:19 +0900

    Update items table and create categories table

--- go/db/items.sql
+++ go/db/items.sql
@@ -1,6 +1,14 @@
+-- items table
 CREATE TABLE IF NOT EXISTS items (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    category TEXT,
-    image_name TEXT
+    category_id INTEGER NOT NULL,
+    image_name TEXT,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+-- categories table
+CREATE TABLE IF NOT EXISTS categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE
 );
